341faa968ab63f366f5da3515a2633e5d85c18f2
MdNaimRipto <naimurtsc567@gmail.com>
2023-02-20 21:18:25 +0600

    Something changed

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ require("dotenv").config()
 
 // middle wares
 const corsOptions = {
-  origin: "*",
-  credentials: true,
-  optionSuccessStatus: 200,
+    origin: "*",
+    credentials: true,
+    optionSuccessStatus: 200,
 };
 
 app.use(cors(corsOptions));
@@ -20,26 +20,26 @@ app.get('/', (req, res) => {
 })
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.1rqmivg.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  serverApi: ServerApiVersion.v1,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverApi: ServerApiVersion.v1,
 });
 
 
 async function run() {
-  try {
-    const programsCollection = client.db("HeartToServe").collection("programs");
-    const eventsCollection = client.db("HeartToServe").collection("events");
-    const donorsCollection = client.db("HeartToServe").collection("donors");
+    try {
+        const programsCollection = client.db("HeartToServe").collection("programs");
+        const eventsCollection = client.db("HeartToServe").collection("events");
+        const donorsCollection = client.db("HeartToServe").collection("donors");
 
-    app.get("/programs", async (req, res) => {
-      const query = {};
-      const programs = await programsCollection.find(query).toArray();
-      res.send(programs);
-    });
-  } finally {
-    
-}
+        app.get("/programs", async (req, res) => {
+            const query = {};
+            const programs = await programsCollection.find(query).toArray();
+            res.send(programs);
+        });
+    } finally {
+
+    }
 }
 
 run().catch((err) => console.error(err));
